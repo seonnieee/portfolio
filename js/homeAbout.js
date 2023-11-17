d5cac93adde2d663e71ab1a53af5c7bb1ce5c48d
LEE Kiseon <139195232+seonnieee@users.noreply.github.com>
2023-11-17 22:52:29 +0900

    Delete js directory

--- js/homeAbout.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.about_icon > li').mouseover(function(){
-        $(this).children('.burble').stop().fadeIn();
-    }).mouseout(function(){
-        $(this).children('.burble').stop().fadeOut('slow');
-    })
-})
